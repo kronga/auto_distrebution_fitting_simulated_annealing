f6f3b7d043ac25db939a49a7588728dbaa7157cf
kronga <kingkrong@gmail.com>
2022-03-19 15:18:56 +0200

    create datasets

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ import os
 import json
 import pandas as pd
 from glob import glob
+import numpy as np
+import random
 
 # project imports
 from algo import Algo
@@ -23,6 +25,31 @@ class Main:
 
     def __init__(self):
         pass
+
+    @staticmethod
+    def finalData(num_dfs):
+        df_list = []
+        for _ in range(num_dfs):
+            df_list.append(Main.createTestData())
+        [df.to_csv(os.path.join(Main.DATA_FOLDER_PATH, "df_{}.csv".format(i)), index=False) for i, df in enumerate(df_list)]
+
+    @staticmethod
+    def createTestData():
+        """
+        Building the datasets for evaluating the method of simulated anealing 
+        """
+        const = random.uniform(-10, 10)
+        row_num = random.randint(100, 10000)
+        col_num = random.randint(5, 15)
+        full_df = pd.DataFrame([const for _ in range(row_num)])
+        normal_idx = random.sample(range(col_num), col_num // 2)
+        for i in range(col_num):
+            if i in normal_idx:
+                full_df[i] = pd.DataFrame(np.random.normal(random.uniform(-100, 100), random.uniform(0, 0), row_num))
+            else:
+                const = random.uniform(-10, 10)
+                full_df[i] = pd.DataFrame([const for i in range(row_num)])
+        return full_df
 
     @staticmethod
     def run_example():
@@ -48,4 +75,5 @@ class Main:
 
 
 if __name__ == '__main__':
-    Main.run_example()
+    # Main.run_example()
+    Main.finalData(50)
